fb239faea6f4231ee2adfdc411279e6d057e6a28
Divy <divy@pop-os.localdomain>
2023-12-02 16:49:42 +0000

    feat: solution for day 1 challenge 1 - goldstart

--- 2023/go/day1/day1.go
+++ 2023/go/day1/day1.go
@@ -2,12 +2,42 @@ package day1
 
 import (
 	"fmt"
+	"strings"
 )
+
+func isDigit(c rune) bool {
+	return (c >= '0' && c <= '9')
+}
+
+func getNumber(str string) int {
+	num := 0
+
+	index := strings.LastIndexFunc(str, isDigit)
+	if index == -1 {
+		return 0
+	}
+	num += int(str[index]) - '0'
+	for _, c := range str {
+		if c >= '0' && c <= '9' {
+			num += 10 * (int(c) - '0')
+			break
+		}
+	}
+	return num % 100
+}
 
 // challenge description https://adventofcode.com/2023/day/1
 func Day1(raw_input string) {
 	// Raw Input for this challenge is a strings separated by new lines.
 
-	// strings := strings.Split(raw_input, "\n")
-	fmt.Printf("Day 1 Challenge! %q")
+	strs := strings.Split(raw_input, "\n")
+	sum := 0
+
+	for _, v := range strs {
+		num := getNumber(v)
+		sum += num
+	}
+
+	fmt.Println("Solution:")
+	fmt.Println(sum)
 }
