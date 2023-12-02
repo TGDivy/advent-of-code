630f897f07553f4c367393bdd5ec716da765db6f
Divy <divy@pop-os.localdomain>
2023-12-02 16:02:14 +0000

    feat: ability to download and read input, and cache it for future use!

--- 2023/go/day1/day1.go
+++ 2023/go/day1/day1.go
@@ -2,13 +2,12 @@ package day1
 
 import (
 	"fmt"
-	"strings"
 )
 
 // challenge description https://adventofcode.com/2023/day/1
 func Day1(raw_input string) {
 	// Raw Input for this challenge is a strings separated by new lines.
 
-	strings := strings.Split(raw_input, "\n")
-	fmt.Printf("Day 1 Challenge! %q", strings)
+	// strings := strings.Split(raw_input, "\n")
+	fmt.Printf("Day 1 Challenge! %q")
 }
--- 2023/go/main.go
+++ 2023/go/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
@@ -27,6 +29,12 @@ func getInput(day int, year int, inputs_path string, cookie string) string {
 
 	filename := filepath.Join(inputs_path, fmt.Sprintf("inputs/%d/day_%02d", year, day))
 
+	data, err := os.ReadFile(filename)
+	if err == nil {
+		log.Printf("Input read from file: %s", filename)
+		return string(data)
+	}
+
 	fmt.Println(inputs_path, filename)
 	url := fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", year, day)
 	body := GetWithAOCCookie(url, cookie)
@@ -40,21 +48,25 @@ func main() {
 	log.SetFlags(0)
 
 	challengesMap := buildChallenges()
-	day := 1
-	cookie := "123213"
+	year := flag.Int("year", 2023, "year of advent-of-code")
+	day := flag.Int("day", 0, "day of advent-of-code")
+	cookie := flag.String("cookie", "SET_COOKIE", "cookie for advent-of-code")
+
+	flag.Parse()
+
 	inputs_path_raw, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()
 	if err != nil {
 		panic(fmt.Sprintf("Unable to find the path to inputs folder %s", err))
 	}
 	inputs_path := strings.TrimSpace(string(inputs_path_raw))
 
-	function, ok := challengesMap[day]
+	function, ok := challengesMap[*day]
 
 	if !ok {
-		log.Fatal("challenge not initialized for: Day-", day)
+		log.Fatal("challenge not initialized for: Day-", *day)
 	}
 
-	raw_input := getInput(day, 2023, inputs_path, cookie)
+	raw_input := getInput(*day, *year, inputs_path, *cookie)
 
 	function(raw_input)
 }
