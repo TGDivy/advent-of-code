6338fa42208186e8a52b3c7549c9643e2f2cf4eb
Divy <divy@pop-os.localdomain>
2023-12-02 16:06:21 +0000

    feat: allow a parameter to refetch the input for a given day

--- 2023/go/main.go
+++ 2023/go/main.go
@@ -24,15 +24,17 @@ func buildChallenges() ChallengesMap {
 	return cmap
 }
 
-func getInput(day int, year int, inputs_path string, cookie string) string {
+func getInput(day int, year int, inputs_path string, cookie string, force_fetch bool) string {
 	log.Printf("Fetching for day %d, year %d", day, year)
 
 	filename := filepath.Join(inputs_path, fmt.Sprintf("inputs/%d/day_%02d", year, day))
 
-	data, err := os.ReadFile(filename)
-	if err == nil {
-		log.Printf("Input read from file: %s", filename)
-		return string(data)
+	if !force_fetch {
+		data, err := os.ReadFile(filename)
+		if err == nil {
+			log.Printf("Input read from file: %s", filename)
+			return string(data)
+		}
 	}
 
 	fmt.Println(inputs_path, filename)
@@ -51,7 +53,7 @@ func main() {
 	year := flag.Int("year", 2023, "year of advent-of-code")
 	day := flag.Int("day", 0, "day of advent-of-code")
 	cookie := flag.String("cookie", "SET_COOKIE", "cookie for advent-of-code")
-
+	force_fetch := flag.Bool("force_fetch", false, "force fetch from advent-of-code server")
 	flag.Parse()
 
 	inputs_path_raw, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()
@@ -66,7 +68,7 @@ func main() {
 		log.Fatal("challenge not initialized for: Day-", *day)
 	}
 
-	raw_input := getInput(*day, *year, inputs_path, *cookie)
+	raw_input := getInput(*day, *year, inputs_path, *cookie, *force_fetch)
 
 	function(raw_input)
 }
