f3c27e5400e0cd3dbbbc0e88066650b9665f6353
tgdivy <me@divyb.xyz>
2023-12-28 08:26:56 +0530

    feat: Ways to win for the corrected race! 2 stars day 6

--- 2023/go/day6/day6.go
+++ 2023/go/day6/day6.go
@@ -42,6 +42,25 @@ func parseData(raw_input string) []Race {
 	return races
 }
 
+func parseDateFixedKerning(raw_input string) Race {
+	var race Race
+	lines := strings.Split(raw_input, "\n")
+
+	times := strings.Split(strings.TrimSpace(strings.Split(lines[0], ":")[1]), " ")
+	distances := strings.Split(strings.TrimSpace(strings.Split(lines[1], ":")[1]), " ")
+
+	times = filter(times, func(s string) bool { return s != "" })
+	distances = filter(distances, func(s string) bool { return s != "" })
+
+	time := strings.Join(times, "")
+	distance := strings.Join(distances, "")
+
+	race.Time, _ = strconv.Atoi(time)
+	race.Distance, _ = strconv.Atoi(distance)
+
+	return race
+}
+
 func possibleWaysToWin(race Race) int {
 	count := 0
 
@@ -61,5 +80,8 @@ func Main(raw_input string) {
 		m1 *= possibleWaysToWin(race)
 	}
 	fmt.Println("Solution: ")
-	fmt.Print("S1: ", m1)
+	fmt.Println("S1: ", m1)
+	correctRace := parseDateFixedKerning(raw_input)
+	s2 := possibleWaysToWin(correctRace)
+	fmt.Println("S2: ", s2)
 }
