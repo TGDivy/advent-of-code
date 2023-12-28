0b196446a05e8b7c9ac58eb13b88fbecb59f1985
tgdivy <me@divyb.xyz>
2023-12-28 06:49:53 +0530

    feat: parsing and recursing through the mappings

--- 2023/go/day5/day5.go
+++ 2023/go/day5/day5.go
@@ -7,13 +7,25 @@ import (
 	"strings"
 )
 
-type mapping struct {
+type RangeMapping struct {
 	DestinationStart int
 	SourceStart      int
 	RangeLen         int
 }
 
-func parse(raw_input string) ([]int, map[string][]mapping) {
+type CategoryMapping struct {
+	from   string
+	to     string
+	ranges []RangeMapping
+}
+
+type Almanac struct {
+	seeds        []int
+	categoryMaps map[string]CategoryMapping
+}
+
+func parse(raw_input string) Almanac {
+	var almanac Almanac
 	parts := strings.Split(raw_input, "\n\n")
 
 	seedsStr := strings.Split(strings.TrimSpace(strings.Split(parts[0], ":")[1]), " ")
@@ -21,33 +33,63 @@ func parse(raw_input string) ([]int, map[string][]mapping) {
 	for i, str := range seedsStr {
 		seeds[i], _ = strconv.Atoi(str)
 	}
-	maps := make(map[string][]mapping)
+	almanac.seeds = seeds
+
+	maps := make(map[string]CategoryMapping)
 
 	for i := 1; i < len(parts); i += 1 {
+		var categoryMap CategoryMapping
 		lines := strings.Split(parts[i], "\n")
 		name := strings.Split(lines[0], " ")[0]
+		nameParts := strings.Split(name, "-")
+		categoryMap.to, categoryMap.from = nameParts[2], nameParts[0]
 
-		var mappings []mapping
-
+		var mappings []RangeMapping
 		for j := 1; j < len(lines); j += 1 {
-			var m mapping
+			var m RangeMapping
 			values := strings.Split(strings.TrimSpace(lines[j]), " ")
-
 			m.DestinationStart, _ = strconv.Atoi(values[0])
 			m.SourceStart, _ = strconv.Atoi(values[1])
 			m.RangeLen, _ = strconv.Atoi(values[2])
-
 			mappings = append(mappings, m)
 		}
+		categoryMap.ranges = mappings
 
-		maps[name] = mappings
+		maps[categoryMap.from] = categoryMap
 	}
 
-	return seeds, maps
+	return almanac
 }
 
-func minCategory([]int, map[string][]mapping) int {
+func mapper(categoryMap CategoryMapping, seed int) int {
+	for _, rangeMap := range categoryMap.ranges {
+		if seed >= rangeMap.SourceStart && seed < (rangeMap.SourceStart+rangeMap.RangeLen) {
+			return rangeMap.DestinationStart + seed - rangeMap.SourceStart
+		}
+	}
+	return seed
+}
+
+func rec(alamanc Almanac, categoryMap CategoryMapping, end string, seed int) int {
+	newValue := mapper(categoryMap, seed)
+	if categoryMap.to == end {
+		return newValue
+	}
+	return rec(alamanc, alamanc.categoryMaps[categoryMap.to], end, newValue)
+}
+
+func minCategory(alamanc Almanac) int {
+	start, end := "seed", "location"
+
+	mappedSeeds := make([]int, len(alamanc.seeds))
+
 	minV := math.MaxInt
+	for i, seed := range alamanc.seeds {
+		mappedSeeds[i] = rec(alamanc, alamanc.categoryMaps[start], end, seed)
+		if mappedSeeds[i] < minV {
+			minV = mappedSeeds[i]
+		}
+	}
 
 	return minV
 }
@@ -55,9 +97,9 @@ func minCategory([]int, map[string][]mapping) int {
 func Main(raw_input string) {
 	fmt.Println(raw_input)
 
-	seeds, mappings := parse(raw_input)
+	almanac := parse(raw_input)
 
-	s1 := minCategory(seeds, mappings)
+	s1 := minCategory(almanac)
 
 	fmt.Println("Solution: ")
 	fmt.Println(s1)
