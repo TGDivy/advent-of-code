0fb0449aed2dccffb8ac097e7ca9be688c6abc97
tgdivy <me@divyb.xyz>
2023-12-28 07:39:31 +0530

    feat: Brute force for 2nd gold star and find the minimum location value for the seeds!

--- 2023/go/day5/day5.go
+++ 2023/go/day5/day5.go
@@ -5,6 +5,8 @@ import (
 	"math"
 	"strconv"
 	"strings"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 type RangeMapping struct {
@@ -76,7 +78,6 @@ func rec(alamanc Almanac, categoryMap CategoryMapping, end string, seed int) int
 	if categoryMap.to == end {
 		return newValue
 	}
-	fmt.Println(categoryMap.to, categoryMap.from)
 	categoryMapNew, ok := alamanc.categoryMaps[categoryMap.to]
 	if !ok {
 		return 0
@@ -84,26 +85,53 @@ func rec(alamanc Almanac, categoryMap CategoryMapping, end string, seed int) int
 	return rec(alamanc, categoryMapNew, end, newValue)
 }
 
-func minCategory(alamanc Almanac) int {
+func minCategory(alamanc Almanac) (int, int) {
 	start, end := "seed", "location"
 
-	mappedSeeds := make([]int, len(alamanc.seeds))
+	bar := progressbar.Default(int64(len(alamanc.seeds)))
 
 	minV := math.MaxInt
-	for i, seed := range alamanc.seeds {
-		mappedSeeds[i] = rec(alamanc, alamanc.categoryMaps[start], end, seed)
-		if mappedSeeds[i] < minV {
-			minV = mappedSeeds[i]
+	for _, seed := range alamanc.seeds {
+		v := rec(alamanc, alamanc.categoryMaps[start], end, seed)
+		if v < minV {
+			minV = v
+		}
+		bar.Add(1)
+	}
+
+	total := 0
+	for i, v := range alamanc.seeds {
+		if i%2 == 0 {
+		} else {
+			total += v
 		}
 	}
 
-	return minV
+	bar2 := progressbar.Default(int64(total))
+
+	minV2 := math.MaxInt
+	rangeStart := 0
+	for i, v := range alamanc.seeds {
+		if i%2 == 0 {
+			rangeStart = v
+		} else {
+			for seed := 0; seed <= v; seed++ {
+				v := rec(alamanc, alamanc.categoryMaps[start], end, seed+rangeStart)
+				if v < minV2 {
+					minV2 = v
+				}
+				bar2.Add(1)
+			}
+		}
+	}
+	return minV, minV2
 }
 
 func Main(raw_input string) {
 	almanac := parse(raw_input)
-	s1 := minCategory(almanac)
+	s1, s2 := minCategory(almanac)
 
 	fmt.Println("Solution: ")
 	fmt.Println(s1)
+	fmt.Println(s2)
 }
